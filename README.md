07fcd165e2289b7e27ec3a9a68537fa47373635f
Tara Dia <taracdia@gmail.com>
2020-11-10 07:29:32 -0600

    changed test pattern and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # To Do List created to learn React Testing better
+
+following https://www.smashingmagazine.com/2020/07/react-apps-testing-library/
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("<App />", () => {
+  it("Renders <App /> component correctly", () => {
+    render(<App />);
+    const h2Element = screen.getByText(/Getting started with react testing library/i);
+    expect(h2Element).toBeInTheDocument();
+  });
 });
